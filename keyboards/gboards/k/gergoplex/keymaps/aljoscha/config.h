005ff6e2b856d10ee119d1642b6893e6d8fc953e
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2021-05-11 10:07:56 +0200

    aljoscha: customize tab-hold config

--- keyboards/gboards/k/gergoplex/keymaps/aljoscha/config.h
+++ keyboards/gboards/k/gergoplex/keymaps/aljoscha/config.h
@@ -1,2 +1,8 @@
 #define COMBO_ALLOW_ACTION_KEYS 
 #define COMBO_VARIABLE_LEN
+
+// Pick good defaults for enabling homerow modifiers
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
